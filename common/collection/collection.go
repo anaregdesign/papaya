c3eefd6d21026e65b4a499e64badd570787c61f2
piroyoung <piroyoung@users.noreply.github.com>
2023-04-27 08:34:58 +0900

    add: Map

--- common/collection/collection.go
+++ common/collection/collection.go
@@ -1,37 +1,41 @@
 package collection
 
 import (
-	"github.com/anaregdesign/papaya/common/concurrent"
+	"context"
 	"github.com/anaregdesign/papaya/common/model"
+	"golang.org/x/sync/semaphore"
 	"runtime"
+	"sync"
 )
 
-func ForEach[T any](slice []T, consumer model.Consumer[T]) {
-	s := concurrent.NewAwaitableSemaphore(runtime.NumCPU())
-
+func ForEach[T any](ctx context.Context, slice []T, consumer model.Consumer[T]) {
+	var wg sync.WaitGroup
+	sem := semaphore.NewWeighted(int64(runtime.NumCPU()))
 	for _, element := range slice {
-		s.Acquire()
-		go func() {
-			defer s.Release()
-			consumer(element)
-		}()
+		wg.Add(1)
+		sem.Acquire(ctx, 1)
+		go func(e T) {
+			defer sem.Release(1)
+			defer wg.Done()
+			consumer(e)
+		}(element)
 	}
-
-	s.Wait()
+	wg.Wait()
 }
 
-func Map[S, T any](slice []S, function model.Function[S, T]) []T {
-	s := concurrent.NewAwaitableSemaphore(runtime.NumCPU())
+func Map[S any, T any](ctx context.Context, slice []S, function model.Function[S, T]) []T {
+	var wg sync.WaitGroup
+	sem := semaphore.NewWeighted(int64(runtime.NumCPU()))
 	result := make([]T, len(slice))
-
 	for i, element := range slice {
-		s.Acquire()
-		go func(i int, element S) {
-			defer s.Release()
-			result[i] = function(element)
+		wg.Add(1)
+		sem.Acquire(ctx, 1)
+		go func(i int, e S) {
+			defer sem.Release(1)
+			defer wg.Done()
+			result[i] = function(e)
 		}(i, element)
 	}
-
-	s.Wait()
+	wg.Wait()
 	return result
 }
---
+++ common/collection/collection_test.go
@@ -0,0 +1,39 @@
+package collection
+
+import (
+	"context"
+	"testing"
+)
+
+func TestForEach(t *testing.T) {
+	ctx := context.Background()
+	var a [32]int
+	for i := 0; i < len(a); i++ {
+		a[i] = i
+	}
+
+	t.Run("valid case", func(t *testing.T) {
+		ForEach(ctx, a[:], func(i int) {
+			t.Log(i)
+		})
+	})
+}
+
+func TestMap(t *testing.T) {
+	ctx := context.Background()
+	var a [32]int
+	for i := 0; i < len(a); i++ {
+		a[i] = i
+	}
+	t.Run("valid case", func(t *testing.T) {
+		b := Map(ctx, a[:], func(i int) int {
+			return i * 2
+		})
+
+		for i := 0; i < len(b); i++ {
+			if b[i] != a[i]*2 {
+				t.Errorf("Map() = %v, want %v", b[i], a[i]*2)
+			}
+		}
+	})
+}
