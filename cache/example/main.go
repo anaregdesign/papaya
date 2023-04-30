6513b96acb2a5d84a17929993378cb63afa3a124
piroyoung <piroyoung@users.noreply.github.com>
2023-04-30 10:31:07 +0900

    add: example

---
+++ cache/example/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"github.com/anaregdesign/papaya/cache"
+	"log"
+	"time"
+)
+
+func main() {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	c := cache.NewCache[string, string](ctx, 1*time.Second)
+
+	// Set a value
+	c.Set("key", "value")
+
+	// Get a value
+	if value, ok := c.Get("key"); ok {
+		log.Printf("Value: %v", value)
+	}
+
+	// Wait for the cache to expire
+	time.Sleep(1 * time.Second)
+
+	// Get a value
+	if _, ok := c.Get("key"); !ok {
+		log.Printf("Key expired")
+	}
+
+	<-ctx.Done()
+
+}
