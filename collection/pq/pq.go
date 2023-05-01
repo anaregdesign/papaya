d7a48c17ffd7be28c42392d04135825e3d4255e7
piroyoung <piroyoung@users.noreply.github.com>
2023-05-02 04:13:53 +0900

    fix: generics

--- collection/pq/pq.go
+++ collection/pq/pq.go
@@ -1,42 +1,49 @@
 package pq
 
-import "container/heap"
+import (
+	"container/heap"
+	"golang.org/x/exp/constraints"
+)
 
 /*
  * This code is from https://golang.org/pkg/container/heap/
  */
 
-type Item[T any] struct {
-	value    T       // The value of the item; arbitrary.
-	priority float64 // The priority of the item in the queue.
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+type Item[S any, T Number] struct {
+	value    S // The value of the item; arbitrary.
+	priority T // The priority of the item in the queue.
 	// The index is needed by update and is maintained by the heap.Interface methods.
 	index int // The index of the item in the heap.
 }
 
 // A PriorityQueue implements heap.Interface and holds Items.
-type PriorityQueue[T any] []*Item[T]
+type PriorityQueue[S any, T Number] []*Item[S, T]
 
-func (pq PriorityQueue[T]) Len() int { return len(pq) }
+func (pq PriorityQueue[S, T]) Len() int { return len(pq) }
 
-func (pq PriorityQueue[T]) Less(i, j int) bool {
+func (pq PriorityQueue[S, T]) Less(i, j int) bool {
 	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
 	return pq[i].priority > pq[j].priority
 }
 
-func (pq PriorityQueue[T]) Swap(i, j int) {
+func (pq PriorityQueue[S, T]) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
 	pq[i].index = i
 	pq[j].index = j
 }
 
-func (pq *PriorityQueue[T]) Push(x any) {
+func (pq *PriorityQueue[S, T]) Push(x any) {
 	n := len(*pq)
-	item := x.(*Item[T])
+	item := x.(*Item[S, T])
 	item.index = n
 	*pq = append(*pq, item)
 }
 
-func (pq *PriorityQueue[T]) Pop() any {
+func (pq *PriorityQueue[S, T]) Pop() any {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
@@ -47,7 +54,7 @@ func (pq *PriorityQueue[T]) Pop() any {
 }
 
 // update modifies the priority and value of an Item in the queue.
-func (pq *PriorityQueue[T]) update(item *Item[T], value T, priority float64) {
+func (pq *PriorityQueue[S, T]) update(item *Item[S, T], value S, priority T) {
 	item.value = value
 	item.priority = priority
 	heap.Fix(pq, item.index)
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.20
 
 require (
 	github.com/google/uuid v1.3.0
+	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53
 	golang.org/x/sync v0.1.0
 )
