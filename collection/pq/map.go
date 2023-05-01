a83d68783e974bf49d32423bc68c9bcbe57cdb44
piroyoung <piroyoung@users.noreply.github.com>
2023-05-02 04:47:46 +0900

    add: filter map

---
+++ collection/pq/map.go
@@ -0,0 +1,25 @@
+package pq
+
+import "container/heap"
+
+func FilterMap[S comparable, T Number](m map[S]T, top int) map[S]T {
+	pq := make(PriorityQueue[S, T], len(m))
+
+	i := 0
+	for k, v := range m {
+		pq[i] = &Item[S, T]{
+			value:    k,
+			priority: v,
+			index:    i,
+		}
+		i++
+	}
+	heap.Init(&pq)
+
+	filtered := make(map[S]T)
+	for i := 0; i < top; i++ {
+		item := heap.Pop(&pq).(*Item[S, T])
+		filtered[item.value] = item.priority
+	}
+	return filtered
+}
